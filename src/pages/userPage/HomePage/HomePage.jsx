47c6d5a995d623db2309cb7bfee98d2ec4fbce88
changs0124 <haeya0124@gmail.com>
2024-10-01 02:01:43 +0900

    컴포넌트 페이지 재구성 및 관리자 페이지 디자인 시작

--- src/pages/userPage/HomePage/HomePage.jsx
+++ src/pages/userPage/HomePage/HomePage.jsx
@@ -28,7 +28,7 @@ function HomePage(props) {
 
     const handleSigninModalOnClick = () => {
         // setSigninModalState(true)
-        nav("/dashboard/*", { replace: true })
+        nav("/dashboard", { replace: true })
     }
 
     const handleSignupModalOnClick = () => {
@@ -42,43 +42,45 @@ function HomePage(props) {
 
     return (
         <MainLayout>
-        <MainContainer>
-            {
-                isLoading ? <LoadingPage /> :
-                    <div css={s.layout}>
-                        <div css={s.container}>
-                            <div css={s.titleBox}>
-                                <h1>MEDIBOOK</h1>
+            <MainContainer>
+                {
+                    isLoading ? <LoadingPage /> :
+                        <div css={s.layout}>
+                            <div css={s.container}>
+                                <div css={s.titleBox}>
+                                    <h1>MEDIBOOK</h1>
+                                </div>
+                                <div css={s.contentBox}>
+                                    <p>MEDIBOOK</p>
+                                    <p>예약 사이트 어플</p>
+                                </div>
+                                <div css={s.buttonBox}>
+                                    <RiHome2Line />
+                                    <button>메인화면 바로가기</button>
+                                </div>
+                                <div css={s.imgBox}>
+                                    <img src="/cloud.png" alt="" />
+                                </div>
                             </div>
-                            <div css={s.contentBox}>
-                                <p>MEDIBOOK</p>
-                                <p>예약 사이트 어플</p>
-                            </div>
-                            <div css={s.buttonBox}>
-                                <RiHome2Line />
-                                <button>메인화면 바로가기</button>
-                            </div>
-                            <div css={s.imgBox}>
-                                <img src="/cloud.png" alt="" />
+                            <div css={s.loginContainer}>
+                                <div css={s.loginTitle}>
+                                    <p>로그인 & 회원가입</p>
+                                </div>
+                                <div css={s.loginButtonBox}>
+                                    <button onClick={handleSigninModalOnClick}>로그인</button>
+                                    <button onClick={handleSignupModalOnClick}>회원가입</button>
+                                    <button onClick={handleAdminSigninModalOnClick}>관리자 로그인</button>
+                                </div>
                             </div>
                         </div>
-                        <div css={s.loginContainer}>
-                            <div css={s.loginTitle}>
-                                <p>로그인 & 회원가입</p>
-                            </div>
-                            <div css={s.loginButtonBox}>
-                                <button onClick={handleSigninModalOnClick}>로그인</button>
-                                <button onClick={handleSignupModalOnClick}>회원가입</button>
-                                <button onClick={handleAdminSigninModalOnClick}>관리자 로그인</button>
-                            </div>
-                        </div>
-                    </div>
-            }
+                }
+            </MainContainer>
             <Routes>
-                <Route path='/dashboard/*' element={<DashBoard />}/>
+                <Route path='/dashboard/*' element={<DashBoard />} />
             </Routes>
-        </MainContainer>
         </MainLayout>
+
+
     );
 }
 
